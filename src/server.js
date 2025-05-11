71629c9439e2b64e39d5a3c435deae8c54e206a0
Sega <brajicsergej@gmail.com>
2025-05-11 12:14:43 +0200

    Server-side routes defined

--- src/server.js
+++ src/server.js
@@ -4,3 +4,34 @@ const app = express();
 app.use(express.static("public"));
 
 app.listen(3000);
+
+app.use(express.static(path.join(__dirname, "../public"))); // Serve static files
+
+app
+  .get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+  })
+  .get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "about.html"));
+  })
+  .get("/FAQs", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "faqs.html"));
+  })
+  .get("/instructions", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "instructions.html"));
+  })
+  .get("/yt-to-mp3", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "yttomp3.html"));
+  })
+  .get("/terms-of-service", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "tos.html"));
+  })
+  .get("/spotify-downloader", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "spotifydownloader.html"));
+  })
+  .get("/video-downloader", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "videodownloader.html"));
+  });
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "../public", "404.html"));
+});
